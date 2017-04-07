497822e5fffa56edc9ab3aa30bfad1103cd28264
nikanorivanovitch <andre.jacques64@gmail.com>
2017-04-07 09:54:44 +0200

    Add files via upload

---
+++ viva Beta.py
@@ -0,0 +1,30 @@
+from tkinter import *
+import time
+import random           
+
+def piece(a):
+    n=0
+    while n==0:
+        hp=random.randint(2,8)
+        lp=(4/3)*hp
+        for x in range(1,h+1,0.1):
+            for y in range(1,l+1,0.1):
+                for k
+                
+        
+        
+
+fenetre=Tk()
+canvas = Canvas(fenetre, width=1000, height=900, background='yellow')
+canvas.pack()
+        
+a=[]
+h=12
+l=8
+k=40
+n_piece=5
+canvas.create_rectangle(10,10,k*l+10,k*h+10)
+a.append[
+
+
+
