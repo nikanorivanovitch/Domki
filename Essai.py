7d1fbcdcc628889502130a73224e5dd62811079d
Paulittis <royer.paul.8@gmail.com>
2017-04-07 08:24:34 +0200

    Update Essai.py

--- Essai.py
+++ Essai.py
@@ -1,3 +1,13 @@
 from tkinter import *
+from PIL import Image, ImageTk
 import time
 import random
+
+
+metre=40
+h=metre*12
+l=metre*9 
+
+fenetre=TK()
+page=Canvas(fenetre, width=h+20, height=l+20)
+page.pack() 
